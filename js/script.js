cdcd3ffa747fe226cecfd6bdc618984dc45e10b0
dasha1917 <dasha.g1917@gmail.com>
2021-02-16 00:30:22 +0200

    Added meta tags, initialized wow.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+new WOW().init();
+
 $(".scroll").click(function (e) {
     var linkHref = $(this).attr("href");
     $("html, body").animate({
-            scrollTop: $(linkHref).offset().top - headerHeight,
+            scrollTop: $(linkHref).offset().top,
         },
         1000
     );
